6f0adae5b97d241a4bfb64a97ee5157c9e4d9f9b
DmytroProg <dmytroprog@gmail.com>
2023-08-28 15:27:40 +0300

    done v1.0

--- BookStoreCore/ViewModels/UserMainViewModel.cs
+++ BookStoreCore/ViewModels/UserMainViewModel.cs
@@ -22,13 +22,17 @@ namespace BookStoreCore.ViewModels
         private string _searchText;
         private string _filterAuthor;
         private string _filterGenre;
-        private Func<IEnumerable<BookDetails>, IEnumerable<BookDetails>> FilterBookData;
+        private string _topFilter;
+        private bool _isUpdatingCollection;
+        private Func<IEnumerable<BookDetails>, IEnumerable<BookDetails>> FilterBookData = null!;
 
         public ObservableCollection<BookDetails> BookDetails { get; set; } = null!;
         public ObservableCollection<Order> SavedBooks { get; set; } = null!;
         public ObservableCollection<Order> MyBooks { get; set; } = null!;
         public List<string> Authors { get; set; } = null!;
         public List<string> Genres { get; set; } = null!;
+        private Dictionary<string, Func<IEnumerable<BookDetails>, IEnumerable<BookDetails>>> _topFilters;
+        public List<string> TopFilters { get; set; } = null!;
 
         public string SearchText
         {
@@ -90,6 +94,26 @@ namespace BookStoreCore.ViewModels
             }
         }
 
+        public string TopFilterKey
+        {
+            get => _topFilter;
+            set
+            {
+                _topFilter = value;
+                OnPropertyChanged(nameof(TopFilterKey));
+                if (_topFilter == "-")
+                {
+                    FilterBookData -= GetTopFilter;
+                }
+                else
+                {
+                    if (!FilterBookData.GetInvocationList().Contains(GetTopFilter))
+                        FilterBookData += GetTopFilter;
+                }
+                UpdateCollection();
+            }
+        }
+
         public UserMainViewModel(NavigationStore navigationStore)
         {
             this._bookDetailsService = new BookDetailsService(
@@ -101,11 +125,17 @@ namespace BookStoreCore.ViewModels
             this.MyBooks = new ObservableCollection<Order>(this._bookDetailsService.GetOrders()
                 .Where(x => x.IsPaid));
             var bookService = new BookService(ConfigurationManager.ConnectionStrings["BookStore"].ConnectionString);
-            this.Authors = new List<string>(bookService.GetAuthors().Select(x => x.LastName));
+            this.Authors = new List<string>(bookService.GetAuthors()
+                .OrderByDescending(x => this._bookDetailsService.GetOrders()?
+                .Where(y => y.Book.Author.Id == x.Id).Count())
+                .Select(x => x.LastName));
             this.Authors.Insert(0, "-");
             this.Genres = new List<string>(bookService.GetGenres().Select(x => x.GenreName));
             this.Genres.Insert(0, "-");
             this.FilterBookData = (list) => GetAllBooks();
+            SetupTopFilters();
+            this.TopFilters = new List<string>(this._topFilters.Keys);
+            this.TopFilterKey = "-";
         }
 
         private IEnumerable<BookDetails> GetAllBooks() => this._bookDetailsService.GetAll()
@@ -119,6 +149,8 @@ namespace BookStoreCore.ViewModels
 
         private void UpdateCollection()
         {
+            if (_isUpdatingCollection) return;
+            _isUpdatingCollection = true;
             this.BookDetails.Clear();
             var list = new List<BookDetails>(); 
             if (FilterBookData != null)
@@ -130,10 +162,53 @@ namespace BookStoreCore.ViewModels
                 }
             }
 
+            if(_topFilter != "-")
+            {
+                list = list.Take(5).ToList();
+            }
+
             foreach(var item in list)
             {
                 this.BookDetails.Add(item);
             }
+            _isUpdatingCollection = false;
+        }
+
+        private IEnumerable<BookDetails> GetTopFilter(IEnumerable<BookDetails> books)
+        {
+            return this._topFilters[this.TopFilterKey].Invoke(books);
+        }
+
+        private void SetupTopFilters()
+        {
+            this._topFilters = new Dictionary<string, Func<IEnumerable<BookDetails>, 
+                IEnumerable<BookDetails>>>(); 
+            this._topFilters.Add("-", null);
+            this._topFilters.Add("Top 5 new books", GetTopNewBooks);
+            this._topFilters.Add("Top 5 best books", GetTopBestBooks);
+            this._topFilters.Add("Top genre today", books => GetBestGenre(books, 1));
+            this._topFilters.Add("Top genre this week", books => GetBestGenre(books, 7));
+            this._topFilters.Add("Top genre this month", books => GetBestGenre(books, 30));
+            this._topFilters.Add("Top genre this year", books => GetBestGenre(books, 365));
+        }
+
+        private IEnumerable<BookDetails> GetTopNewBooks(IEnumerable<BookDetails> books) =>
+            books.OrderByDescending(x => x.Book.PublishYear);
+        private IEnumerable<BookDetails> GetTopBestBooks(IEnumerable<BookDetails> books) =>
+            books.OrderByDescending(x => this._bookDetailsService?.GetOrders()?
+            .Where(y => y.Book.Id == x.Book.Id).Count());
+        private IEnumerable<BookDetails> GetBestGenre(IEnumerable<BookDetails> books, int days)
+        {
+            string genre = this.Genres.GroupBy(g => g)
+                .OrderByDescending(x => this._bookDetailsService?.GetOrders()?
+                .Where(y => y.Book.Genres
+                .Select(z => z.GenreName)
+                .Contains(x.Key)
+                && (DateTime.Now - y.OrderDate) <= new TimeSpan(days, 0, 0, 0))
+                .Count()).First().Key;
+
+            this.FilterGenre = genre;
+            return GetBooksByGenre(books);        
         }
 
         public ICommand ChooseBook
